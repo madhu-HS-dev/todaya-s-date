7cd92cf518f52c0aafeef3cd71703e150c734835
madhu <madhusacharya142@gmail.com>
2023-04-03 16:37:29 +0000

    today's date

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  let date = new Date();
+  const TodayDate = `${date.getDate()}-${
+    date.getMonth() + 1
+  }-${date.getFullYear()}`;
+  response.send(TodayDate);
+});
+
+app.listen(3000);
+
+module.exports = app;
